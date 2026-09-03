ffd44cdcba71b159fe1e42740e21ea9d846ba3e3
AnshumanSharma147 <anshumansharma162008@gmail.com>
2026-09-03 18:42:05 +0530

    Add text file handling

--- File Manager CLI/Code.py
+++ File Manager CLI/Code.py
@@ -5,17 +5,27 @@ def create_file(name,type):
     with open(f"{name}.{type}","w"):
         pass
 
+def text_data():
+    data=[]
+    print("How many lines do you want to enter?")
+    limit=int(input(">"))
+    for line in range(limit):
+        print("Enter line {}:".format(line+1))
+        line_data=input("> ")
+        data.append(line_data+"\n")
+    return data
+
 def rows_data():
-    data_list=[]
+    data=[]
     print("Enter number of rows you would like to define: ")
     row_limt=int(input("> "))
     for i in range(row_limt):
         print("Enter row{} data: ".format(i+1))
         row_data=input("> ").split()
-        data_list.append(row_data)
-    return data_list
+        data.append(row_data)
+    return data
 
-def write_rows(data,destination,extension):
+def write_file(data,destination,extension):
     with open(destination,"w",newline="") as file:
         if extension.lower()=="csv":
             writer=csv.writer(file)
@@ -23,30 +33,29 @@ def write_rows(data,destination,extension):
         elif extension.lower()=="txt":
             file.writelines(data)
 
-def append_rows(data,destination,extension):
+def append_file(data,destination,extension):
     with open(destination,"a",newline="") as file:
         if extension.lower()=="csv":
             writer_obj=csv.writer(file)
             writer_obj.writerows(data)
         elif extension.lower()=="txt":
-            #text file code pending...
-            pass
+            file.writelines(data)
 
-def read_rows(destination,extension):
+def read_file(destination,extension):
     with open(destination,"r",newline="") as file:
         if extension.lower()=="csv":
             content_obj=csv.reader(file)
             for row in content_obj:
                 print(row)
         elif extension.lower()=="txt":
-            #text reader is pending..
-            pass
+            print(file.read())
+
 
 
 def operation_create():
     print("Name of the file: ")
     file_name=input("> ")
-    print("File extension(png,jpeg,txt,csv): ")
+    print("File extension(txt,csv): ")
     file_extension=input("> ")
     create_file(file_name,file_extension)
 
@@ -54,20 +63,25 @@ def operation_manage():
     print("What would you like to do: ")
     print("1) Read\n2) Write\n3) Append")
     choice=input("> ")
-    print("which file would you like to edit: ")
+    print("which file would you like to manage: ")
     file_name=input("> ")
     file_extension=file_name.split(".")[-1]
 
     if choice.lower()=="read":
-        read_rows(file_name,file_extension)
+        read_file(file_name,file_extension)
     elif choice.lower()=="write":
-        data_list= rows_data()
-        write_rows(data_list,file_name,file_extension)
+        if file_extension.lower()=="txt":
+            data_list=text_data()
+        elif file_extension.lower()=="csv":
+            data_list= rows_data()
+
+        write_file(data_list,file_name,file_extension)
     elif choice.lower()=="append":
-        print("Would you like to append a row or append rows?")
-        append_choice=input("> ")
-        data_list=rows_data()
-        append_rows(data_list,file_name,file_extension)
+        if file_extension.lower()=="txt":
+            data_list=text_data()
+        elif file_extension.lower()=="csv":
+            data_list=rows_data()
+        append_file(data_list,file_name,file_extension)
 
 
 
